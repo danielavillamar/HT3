d067350aa11cc7eee0f8faec260489017fdfc58b
danielavillamar <46539048+danielavillamar@users.noreply.github.com>
2020-02-11 09:29:10 -0600

    Update RadixSort.java

--- src/application/RadixSort.java
+++ src/application/RadixSort.java
@@ -34,3 +34,26 @@ public class RadixSort {
             output[count[ ((int)arr[i]/exp)%10 ] - 1] = (int)arr[i];
             count[ ((int)arr[i]/exp)%10 ]--;
         }
+	     
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to curent digit
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+ 
+    // The main function to that sorts arr[] of size n using
+    // Radix Sort
+    static Comparable[] radixsort(Comparable arr[], int n)
+    {
+        // Find the maximum number to know number of digits
+        int m = getMax(arr, n);
+ 
+        // Do counting sort for every digit. Note that instead
+        // of passing digit number, exp is passed. exp is 10^i
+        // where i is current digit number
+        for (int exp = 1; m/exp > 0; exp *= 10)
+            countSort(arr, n, exp);
+        return arr;
+    }
+
+}
